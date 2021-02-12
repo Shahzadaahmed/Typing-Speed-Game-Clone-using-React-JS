bc1bd47adb7d9a888361e02d8fd6dc8e8482823e
ShahzadaAhmed <shahzada008@hotmail.com>
2021-02-13 02:50:52 +0500

    Updated Typing Speed Game

--- src/TypingSpeedGame.js
+++ src/TypingSpeedGame.js
@@ -23,7 +23,8 @@ class TypingSpeedGame extends Component {
             wordsCount: 0,
             isPlaying: false,
             selectLevel: "Select Level",
-            hasGameOver: true
+            hasGameOver: true,
+            typingLevel: undefined
         }
     }
 
@@ -47,6 +48,8 @@ class TypingSpeedGame extends Component {
         if (sceondsClone < 1) {
             if (minutesClone < 1) {
                 clearInterval(this.timeIntervalOfStartTimer);
+                this.speedTest();
+                // console.log(this.state.typingLevel);
                 this.setState({
                     isPlaying: false,
                     hasGameOver: false,
@@ -163,14 +166,48 @@ class TypingSpeedGame extends Component {
         this.startGame(inputValue);
     }
 
+    // Note (Function # 08): Function to test user typing speed...!
+    speedTest = () => {
+        let wordsCountClone = this.state.wordsCount;
+        let typingLevelClone = this.state.typingLevel;
+
+        if (wordsCountClone >= 20 && wordsCountClone < 45) {
+            typingLevelClone = "Your typing speed is Average!";
+            this.setState({
+                typingLevel: typingLevelClone
+            });
+        }
+
+        else if (wordsCountClone >= 45 && wordsCountClone < 70) {
+            typingLevelClone = "Your typing speed is Fluent!";
+            this.setState({
+                typingLevel: typingLevelClone
+            });
+        }
+
+        else if (wordsCountClone >= 70) {
+            typingLevelClone = "Your typing speed is Super Fast!";
+            this.setState({
+                typingLevel: typingLevelClone
+            });
+        }
+
+        else if (wordsCountClone < 20) {
+            typingLevelClone = "Your typing speed is Very Slow!";
+            this.setState({
+                typingLevel: typingLevelClone
+            });
+        }
+    }
+
     /***** App Functionality Finished *****/
 
-    // Note (Function # 08): Function tp play again...!
+    // Note (Function # 09): Function tp play again...!
     playAgain = () => {
         window.location.reload();
     }
 
-    // Note (Function # 09): Function to close the game...!
+    // Note (Function # 10): Function to close the game...!
     closeGame = () => {
         alert("Game Closed! By Now!!!");
         window.close();
@@ -180,77 +217,85 @@ class TypingSpeedGame extends Component {
     render() {
         return (
             // Main container...!
-            <div>
-                <Container fixed>
+            <React.Fragment>
+                <div>
+                    <Container fixed>
 
-                    <h1 id="appHeader"> Typing Speed Game in React JS </h1>
-                    <hr />
+                        {/* App Header */}
+                        <h1 id="appHeader"> Typing Speed Game in React JS </h1>
+                        <hr />
 
-                    {/* Time and Score Container */}
-                    <div id="middleContainer">
-                        <span id="spanTag">
-                            {
-                                ((this.state.minutes && this.state.seconds) === undefined)
-                                    ?
-                                    ("Time: Minutes: 0 : Seconds: 0")
-                                    :
-                                    (`Time: Minutes: ${this.state.minutes} : Seconds: ${this.state.seconds}`)
-                            }
+                        {/* Inner container # 1 */}
+                        <div id="middleContainer">
+                            <span id="spanTag">
+                                {
+                                    ((this.state.minutes && this.state.seconds) === undefined)
+                                        ?
+                                        ("Time: Minutes: 0 : Seconds: 0")
+                                        :
+                                        (`Time: Minutes: ${this.state.minutes} : Seconds: ${this.state.seconds}`)
+                                }
+                            </span>
+
+                            <span id="spanTag">
+                                You typed {(!this.state.hasGameOver) ? (<span className="totalCountNum"> {this.state.wordsCount} </span>) : (null)} words
                         </span>
-                        <span id="spanTag"> You typed: {this.state.wordsCount} words </span>
-                    </div>
+                        </div>
 
-                    {/* Header */}
-                    <h1 id="gameHeader"> Beat The Word </h1>
+                        {/* Result Header */}
+                        <h1 id="gameHeader">
+                            {(this.state.hasGameOver) ? ("Beat The Word") : (this.state.typingLevel)}
+                        </h1>
 
-                    {/* Game Levels Area */}
-                    <select id="dropdown" value={this.state.selectLevel} onChange={this.selectHandler}>
-                        <option value="SelectLevel"> Select Level </option>
-                        <option value="One"> One Minute Challenge </option>
-                        <option value="Two"> Two Minutes Challenge </option>
-                        <option value="Five"> Five Minutes Challenge </option>
-                    </select>
+                        {/* Game Levels Area */}
+                        <select id="dropdown" value={this.state.selectLevel} onChange={this.selectHandler}>
+                            <option value="SelectLevel"> Select Level </option>
+                            <option value="One"> One Minute Challenge </option>
+                            <option value="Two"> Two Minutes Challenge </option>
+                            <option value="Five"> Five Minutes Challenge </option>
+                        </select>
 
-                    {/* Random Word Area */}
-                    <h2 id="randomWordHeader"> {this.state.randomWord} </h2>
+                        {/* Random Word Area */}
+                        <h2 id="randomWordHeader"> {this.state.randomWord} </h2>
 
-                    {/* Input Field Area */}
-                    {
-                        (this.state.hasGameOver)
-                            ?
-                            (
-                                <div id="inputFieldContainer">
-                                    <TextField
-                                        autoFocus
-                                        label="Type Fast"
-                                        id="standard-basic"
-                                        value={this.state.userInput}
-                                        onChange={this.formHandler}
-                                    />
-                                </div>
-                            )
-                            :
-                            (null)
-                    }
+                        {/* Input Field Area */}
+                        {
+                            (this.state.hasGameOver)
+                                ?
+                                (
+                                    <div id="inputFieldContainer">
+                                        <TextField
+                                            autoFocus
+                                            label="Type Fast"
+                                            id="standard-basic"
+                                            value={this.state.userInput}
+                                            onChange={this.formHandler}
+                                        />
+                                    </div>
+                                )
+                                :
+                                (null)
+                        }
 
-                    {/* Showing Meessage Area */}
-                    <h2 id="messageArea"> {this.state.message} </h2>
+                        {/* Showing Meessage Area */}
+                        <h2 id="messageArea"> {this.state.message} </h2>
 
-                    {/* Instructions Area */}
-                    <div className="alert alert-success" role="alert" id="instrunctionsContainer">
-                        <h4 className="alert-heading"> Read the Instructions Carefully! </h4>
-                        <p> Select the level and check our typing speed level in the given amount of time. Once the game started
-                        you cannot change level. Once the game is over then you need to click play again button to play the game otherwise
+                        {/* Instructions Area */}
+                        <div className="alert alert-success" role="alert" id="instrunctionsContainer">
+                            <h4 className="alert-heading"> Read the Instructions Carefully! </h4>
+                            <p> Select the level and check our typing speed level in the given amount of time. Once the game started
+                            you cannot change level. Once the game is over then you need to click play again button to play the game otherwise
                         field bar and dropdown list will be disable. </p>
-                    </div>
+                        </div>
 
-                    {/* Buttons Area */}
-                    <div id="footerContainer">
-                        <Button title="Play Again" variant="contained" color="secondary" onClick={this.playAgain} id="myButtons"> Play Again </Button>
-                        <Button title="Close Game" variant="contained" color="secondary" onClick={this.closeGame} id="myButtons"> End Game </Button>
-                    </div>
-                </Container>
-            </div>
+                        {/* Buttons Area */}
+                        <div id="footerContainer">
+                            <Button title="Play Again" variant="contained" color="secondary" onClick={this.playAgain} id="myButtons"> Play Again </Button>
+                            <Button title="Close Game" variant="contained" color="secondary" onClick={this.closeGame} id="myButtons"> End Game </Button>
+                        </div>
+                    </Container>
+                </div>
+            </React.Fragment>
         )
     }
 }
